4087a590544b91d9420724598003f91a46864abb
myxvisual <myxvisual@live.com>
2019-05-09 17:31:33 +0800

    feat: Update to version 1.0.7

--- bin/ossr.js
+++ bin/ossr.js
@@ -2,13 +2,14 @@
 'use strict'
 
 var { createReadStream } = require('fs')
-var inquirer = require('inquirer')
 var path = require('path')
 var fs = require('fs')
 var chalk = require('chalk')
 var program = require('commander')
 var OSS = require('ali-oss')
 var AgentKeepAlive = require('agentkeepalive')
+var { Select, Confirm, Input } = require('enquirer')
+const promptProcess = require('./promptProcess')
 
 var argv = process.argv
 var ossClient
@@ -32,62 +33,20 @@ program
   .usage(`${chalk.green('<local-path> [upload-path]')} [...options]`)
 
   .option('-d, --delete [deletePath]', 'delete remote exists path', function(deletePath) {
-    function deleteAllPath(currDeletePath) {
-      if (currDeletePath) {
-        var isFile = currDeletePath.slice(-1) !== '/'
-        if (isFile) {
-          checkConfig(function() {
-            ossClient.delete(currDeletePath)
-          })
-        } else {
-          checkConfig(function() {
-            ossClient.list(({ prefix: currDeletePath, delimiter: '/' }))
-              .then(res => {
-                if (res.objects) {
-                  res.objects.forEach(obj => {
-                    ossClient.delete(obj.name)
-                  })
-                }
-                if (res.prefixes) {
-                  res.prefixes.forEach(newPath => {
-                    deleteAllPath(newPath)
-                  })
-                }
-              })
-              .catch(err => {
-                console.error(err)
-              })
-          })
-        }
-      }
-    }
-    inquirer.prompt({
-      type: 'confirm',
+    var prompt = new Confirm({
       name: 'confirmDelete',
       message: 'Make sure to remote the path: ' + chalk.red(deletePath)
-    }).then(answers => {
-      if (answers.confirmDelete) {
-        deleteAllPath(deletePath)
+    })
+    
+    prompt.run().then(confirm => {
+      if (confirm) {
+        ossDelete(deletePath)
       }
     })
   })
   .option('-l, --list [prefix]', 'list remote prefix files', function(prefix) {
     listAll = false
-    checkConfig(function() {
-      ossClient.list(({ prefix, delimiter: '/' })).then(res => {
-          if (res.objects || res.prefixes) {
-            console.log({
-              prefixes: res.prefixes,
-              objecst: res.objects ? res.objects.map(obj => obj.url) : []
-            })
-          } else {
-            console.error(chalk.red('notfound ' + prefix))
-          }
-        })
-        .catch(err => {
-          console.error(err)
-        })
-    })
+    listByPrefix(prefix)
   })
 
   .option('-r, --region [region]', '[Config] set region')
@@ -121,6 +80,7 @@ function updateOSSClient() {
       ossConfig[key] = program[key]
     }
   })
+
   if (ossConfig.timeout) {
     ossConfig.agent = new AgentKeepAlive({
       timeout: ossConfig.timeout
@@ -138,26 +98,154 @@ function updateOSSClient() {
 program.parse(argv)
 
 if (listAll && argv.includes('-l')) {
+  listByPrefix('')
+} else {
+  updateOSSClient()
+}
+
+function listByPrefix(prefix, selectedPath) {
   checkConfig(function() {
-    ossClient.list(({ prefix: '', delimiter: '/' })).then(res => {
-        if (res.objects) {
-          console.log({
-            prefixes: res.prefixes,
-            objecst: res.objects.map(obj => obj.url)
+    ossClient.list(({ prefix, delimiter: '/' })).then(res => {
+        if (res.objects || res.prefixes) {
+          var prefixes = res.prefixes || []
+          var objects = res.objects || []
+          var typeItems = [{
+            name: prefix,
+            value: prefix,
+            type: 'root',
+            onPress: function() {}
+          }]
+          if (prefix !== '') {
+            typeItems.push({
+              name: '../',
+              value: '',
+              // root | folder | file | action | info
+              type: 'folder',
+              disabled: Boolean(selectedPath),
+              onPress: function() {}
+            })
+          }
+          prefixes.forEach(function(name) {
+            typeItems.push({ name, value: name, type: 'folder', onPress: function() {} })
           })
+          objects.forEach(function(item) {
+            typeItems.push({ name: item.name, value: item.name, type: 'file', onPress: function() {} })
+            if (selectedPath === item.name) {
+              typeItems.push({ name: item.url, value: item.url, type: 'info', onPress: function() {} })
+              // typeItems.push({ name: 'rename', value: 'rename', type: 'action', onPress: function() {} })
+              typeItems.push({ name: 'delete', value: 'delete', type: 'action', onPress: function() {} })
+            }
+          })
+          var typeItemLastIndex = typeItems.length - 1
+          var consoleItems = typeItems.map(function(item, index) {
+            switch (item.type) {
+              case 'root':
+                return chalk.yellow(item.name)
+              case 'action':
+                return chalk.blue('    └── ' + item.name)
+              case 'info':
+                return chalk.grey('    └── ' + item.name)
+              case 'file':
+                var prefixCharacter = typeItemLastIndex === index ? '└── ' : '├── '
+                return chalk.green(prefixCharacter + item.name)
+              case 'folder':
+                var prefixCharacter = typeItemLastIndex === index ? '└── ' : '├── '
+                return  chalk.yellow(prefixCharacter + item.name)
+              default:
+                break
+            }
+          })
+
+          var choices = consoleItems.map(function(consoleItem, index) {
+            var name = typeItems[index].name
+            var type = typeItems[index].type
+            var disabled = type === 'root' || (selectedPath && type !== 'action' && selectedPath !== name)
+            return { name, message: consoleItem, disabled }
+          })
+          var prompt = new Select({
+            name: null,
+            message: prefix,
+            initial: selectedPath,
+            choices
+          })
+          promptProcess(prompt)
+          prompt
+            .run()
+            .then(answerStr => {
+              var index = typeItems.map(function(typeItem) {
+                return typeItem.name
+              }).indexOf(answerStr)
+              var answer = typeItems[index]
+              switch (answer.type) {
+                case 'folder':
+                  if (answer.name === '../') {
+                    var parentPrefix = prefix.split('/')
+                    parentPrefix = parentPrefix.slice(0, parentPrefix.length - 2).join('/')
+                    if (parentPrefix !== '') {
+                      parentPrefix += '/'
+                    }
+                    listByPrefix(parentPrefix)
+                  } else {
+                    listByPrefix(answer.name)
+                  }
+                  break
+                case 'file':
+                  if (answer.name === selectedPath) {
+                    listByPrefix(prefix)
+                  } else {
+                    listByPrefix(prefix, answer.name)
+                  }
+                  break
+                case 'action':
+                  switch (answerStr) {
+                    case 'rename':
+                      var prompt = new Input({
+                        name: 'filename',
+                        message: 'input the new filename'
+                      })
+                      
+                      prompt.run().then(filename => {
+                        console.log('filename:', filename)
+                      })
+                      break
+                    case 'delete':
+                      var prompt = new Confirm({
+                        name: 'confirmDelete',
+                        message: 'Make sure to remote the path: ' + chalk.red(selectedPath)
+                      })
+                      
+                      prompt.run().then(confirm => {
+                        if (confirm) {
+                          ossDelete(selectedPath, () => {
+                            listByPrefix(prefix)
+                          })
+                        } else {
+                          listByPrefix(prefix)
+                        }
+                      })
+                      break
+                    default:
+                      break
+                  }
+                  break
+                default:
+                  break
+              }
+              process.stdin.resume()
+            })
+            .catch(console.error)
         } else {
-          console.error(chalk.red('notfound ' + '/'))
+          console.error(chalk.red('Notfound by prefix: "' + prefix + '"'))
+          listByPrefix('')
         }
       })
       .catch(err => {
         console.error(err)
       })
   })
-} else {
-  updateOSSClient()
 }
 
-function uploadToOSS(absolutePath = '', remotePath = '', options) {
+function ossUpload(absolutePath = '', remotePath = '', options) {
   var isFile = remotePath.slice(-1) !== '/'
   var stream = createReadStream(absolutePath)
   if (options && options.encoding) {
@@ -175,6 +263,41 @@ function uploadToOSS(absolutePath = '', remotePath = '', options) {
       console.log('-- remote path is : '  + chalk.green(res.res.requestUrls))
     })
     .catch(err => console.error(err))
+}
+
+function ossDelete(deletePath, callback) {
+  function deleteAllPath(currDeletePath) {
+    if (currDeletePath) {
+      var isFile = currDeletePath.slice(-1) !== '/'
+      if (isFile) {
+        checkConfig(function() {
+          ossClient.delete(currDeletePath).then(res => {
+            if (callback) callback()
+          })
+        })
+      } else {
+        checkConfig(function() {
+          ossClient.list(({ prefix: currDeletePath, delimiter: '/' }))
+            .then(res => {
+              if (res.objects) {
+                res.objects.forEach(obj => {
+                  ossClient.delete(obj.name)
+                })
+              }
+              if (res.prefixes) {
+                res.prefixes.forEach(newPath => {
+                  deleteAllPath(newPath)
+                })
+              }
+            })
+            .catch(err => {
+              console.error(err)
+            })
+        })
+      }
+    }
+  }
+  deleteAllPath(deletePath)
 }
 
 function uploadFileOrDir(uploadPath = '', remotePath = '', options) {
@@ -198,11 +321,11 @@ function uploadFileOrDir(uploadPath = '', remotePath = '', options) {
         if (isDir) {
           uploadAllPath(newPath, remotePath + file + '/')
         } else {
-          uploadToOSS(newPath, remotePath, options)
+          ossUpload(newPath, remotePath, options)
         }
       })
     } else {
-      uploadToOSS(absolutePath, remotePath, options)
+      ossUpload(absolutePath, remotePath, options)
     }
   }
 
---
+++ bin/promptProcess.js
@@ -0,0 +1,98 @@
+var roles = require('enquirer/lib/roles')
+var utils = require('enquirer/lib/utils')
+var colors = require('ansi-colors')
+
+module.exports = function promptProcess(prompt) {
+  prompt.toChoice = async function(ele, i, parent) {
+    if (typeof ele === 'function') ele = await ele.call(this, this);
+    if (ele instanceof Promise) ele = await ele;
+    if (typeof ele === 'string') ele = { name: ele };
+
+    if (ele.normalized) return ele;
+    ele.normalized = true;
+
+    let origVal = ele.value;
+    let role = roles(ele.role, this.options);
+    ele = role(this, ele);
+
+    if (typeof ele.disabled === 'string' && !ele.hint) {
+      ele.hint = ele.disabled;
+      ele.disabled = true;
+    }
+
+    if (ele.disabled === true && ele.hint == null) {
+      // ele.hint = '(disabled)';
+    }
+
+    // if the choice was already normalized, return it
+    if (ele.index != null) return ele;
+    ele.name = ele.name || ele.key || ele.title || ele.value || ele.message;
+    ele.message = ele.message || ele.name || '';
+    ele.value = [ele.value, ele.name].find(this.isValue.bind(this));
+
+    ele.input = '';
+    ele.index = i;
+    ele.cursor = 0;
+
+    utils.define(ele, 'parent', parent);
+    ele.level = parent ? parent.level + 1 : 1;
+    if (ele.indent == null) {
+      ele.indent = parent ? parent.indent + '  ' : (ele.indent || '');
+    }
+
+    ele.path = parent ? parent.path + '.' + ele.name : ele.name;
+    ele.enabled = !!(this.multiple && !this.isDisabled(ele) && (ele.enabled || this.isSelected(ele)));
+
+    if (!this.isDisabled(ele)) {
+      this.longest = Math.max(this.longest, colors.unstyle(ele.message).length);
+    }
+
+    // shallow clone the choice first
+    let choice = { ...ele };
+
+    // then allow the choice to be reset using the "original" values
+    ele.reset = (input = choice.input, value = choice.value) => {
+      for (let key of Object.keys(choice)) ele[key] = choice[key];
+      ele.input = input;
+      ele.value = value;
+    };
+
+    if (origVal == null && typeof ele.initial === 'function') {
+      ele.input = await ele.initial.call(this, this.state, ele, i);
+    }
+
+    return ele;
+  }
+
+  prompt.render = async function() {
+    let { submitted, size } = this.state;
+  
+    let prompt = '';
+    let header = await this.header();
+    let prefix = await this.prefix();
+    let separator = await this.separator();
+    let message = await this.message();
+  
+    if (this.options.promptLine !== false) {
+      prompt = [prefix, message, separator, ''].join(' ');
+      this.state.prompt = prompt;
+    }
+  
+    let output = await this.format();
+    let help = (await this.error()) || (await this.hint());
+    let body = await this.renderChoices();
+    let footer = await this.footer();
+  
+    if (output) prompt += output;
+    if (help && !prompt.includes(help)) prompt += ' ' + help;
+  
+    if (submitted && !output && !body.trim() && this.multiple && this.emptyError != null) {
+      prompt += this.styles.danger(this.emptyError);
+    }
+  
+    this.clear(size);
+    this.write([header, body, footer].filter(Boolean).join('\n'));
+    this.write(this.margin[2]);
+    this.restore();
+  }
+}
